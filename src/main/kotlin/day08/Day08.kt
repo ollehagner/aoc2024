410456bceedb4bfb3c2bbdccee5fcb695924762f
Olle Hagner <olle.hagner@gmail.com>
2024-12-08 19:57:24 +0100

    Day 08 done

---
+++ src/main/kotlin/day08/Day08.kt
@@ -0,0 +1,59 @@
+package day08
+
+import common.Grid
+import common.Point
+import common.readInput
+
+fun main() {
+    val testinput = readInput("main/kotlin/day08/testinput")
+    val input = readInput("main/kotlin/day08/input")
+
+    val grid = Grid(input) { point, value -> value }
+    println("Part 1 num of antinodes: ${part1(grid)}")
+    println("Part 2 num of antinodes: ${part2(grid)}")
+}
+
+private const val EMPTY = '.'
+
+fun part1(grid: Grid<Char>): Int {
+   val nodePairs = findNodePairs(grid)
+   return nodePairs
+       .map { it.first.move(it.first - it.second) }
+       .filter { grid.hasValue(it) }
+       .distinct()
+       .count()
+}
+
+fun part2(grid: Grid<Char>): Int {
+    val nodePairs = findNodePairs(grid)
+    return (nodePairs
+        .flatMap {
+            findAllInLine(it.first, it.first - it.second, grid).toList()
+        }
+        .toSet() + nodePairs.map { it.first }.toSet()).count()
+}
+
+fun findAllInLine(start: Point, vector: Point, grid: Grid<Char>): Sequence<Point> = sequence {
+    var current = start
+    while(grid.hasValue(current.move(vector))) {
+        current = current.move(vector)
+        yield(current)
+    }
+}
+
+fun findNodePairs(grid: Grid<Char>) : List<Pair<Point, Point>> {
+    return grid.entries()
+        .filter { (_, value) -> value != EMPTY }
+        .groupBy { it.value }
+        .filter { it.value.size > 1 }
+        .mapValues { nodes ->
+            nodes.value
+                .flatMap { (first, _) ->
+                    nodes.value
+                        .filter { it.key != first }
+                        .map { (second, _) ->
+                            first to second
+                        }
+                }
+        }.values.flatten()
+}
