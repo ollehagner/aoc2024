973dea9f409f83c8304250b615c7e0bd83bc661f
Olle Hagner <olle.hagner@gmail.com>
2024-12-20 19:00:42 +0100

    Day 20 done

--- src/main/kotlin/day20/Day20.kt
+++ src/main/kotlin/day20/Day20.kt
@@ -1,6 +1,5 @@
 package day20
 
-import common.Direction
 import common.Grid
 import common.Point
 import common.readInput
@@ -11,62 +10,77 @@ const val WALL = '#'
 fun main() {
     val testinput = readInput("main/kotlin/day20/testinput")
     val input = readInput("main/kotlin/day20/input")
+
     println("Part 1 No of cheats saving 100ps: ${part1(input)}")
+    println("Part 2 No of cheats saving 100ps: ${part2(input)}")
 }
 
 fun part1(input: List<String>): Int {
-    val grid = input.map { it.toCharArray().toList() }.let { Grid(it) }
-    val start = grid.entries().find { it.value == 'S' }!!.key
-    val end = grid.entries().find { it.value == 'E' }!!.key
-    val baseline = solve(start, end, grid)
-    return findPassableWalls(grid)
-        .map { passableWall ->
-            val gridCopy = grid.copy()
-            gridCopy.set(passableWall, '.')
-            solve(start, end, gridCopy)
-        }
-        .groupingBy { baseline - it }.eachCount()
-        .filter { (save, _) -> save >=100 }
-        .map { it.value }
+    return solve(input, 2, 100)
+}
+
+fun part2(input: List<String>): Int {
+    return solve(input, 20, 100)
+}
+
+fun solve(input: List<String>, maxCheatSeconds: Int, minimumSave: Int): Int {
+    val path = traverseTrack(input)
+    val cheats = findShortcuts(path, maxCheatSeconds, minimumSave)
+    return cheats
+        .values
         .sum()
 }
 
-fun solve(start: Point, end: Point, grid: Grid<Char>): Int {
+fun traverseTrack(input: List<String>): Set<PathPosition> {
+    val grid = input.map { it.toCharArray().toList() }.let { Grid(it) }
+    val start = grid.entries().find { it.value == 'S' }!!.key
+    val end = grid.entries().find { it.value == 'E' }!!.key
     val queue = PriorityQueue<PathPosition>( compareBy( { it.minimumExpectedCost} ))
-    queue.add(PathPosition(start, 0, end))
-    val seen = mutableSetOf<Point>()
+    val startPathPosition = PathPosition(start, 0, end)
+    queue.add(startPathPosition)
+    val seen = mutableSetOf<PathPosition>()
+    seen.add(startPathPosition)
     while (queue.isNotEmpty()) {
         val current = queue.poll()
-        if(current.isFinished()) return current.distance
+        if(current.isFinished()) break
         current.position
             .cardinalNeighbors()
-            .filter { it !in seen }
             .filter { grid.valueOf(it) != WALL }
+            .map { current.copy(position = it, distance = current.distance + 1) }
+            .filter { it !in seen }
             .forEach {
                 seen.add(it)
-                queue.add(current.copy(position = it, distance = current.distance + 1))
+                queue.add(it)
             }
     }
-    throw IllegalStateException("Should always have a solution")
+    return seen
 }
 
-fun findPassableWalls(grid: Grid<Char>): Set<Point> {
-    return grid.entries()
-        .filter { (position, _) -> position.x !in setOf(grid.min.x, grid.max.x) && position.y !in setOf(grid.min.y, grid.max.y) }
-        .filter { (_, value) -> value == WALL }
-        .filter { (position, _) ->
-            (grid.valueOf(position.move(Direction.LEFT)) != WALL && grid.valueOf(position.move(Direction.RIGHT)) != WALL)
-                    || (grid.valueOf(position.move(Direction.UP)) != WALL && grid.valueOf(position.move(Direction.DOWN)) != WALL)
-
+fun findShortcuts(path: Set<PathPosition>, maximumCheatSeconds: Int, minimumCheatSave: Int = 0): Map<Int, Int> {
+    return path
+        .flatMap { pathPosition ->
+            path
+                .filter { pathPosition.position.manhattanDistance(it.position) <= maximumCheatSeconds }
+                .map {
+                    val saved = it.distance - pathPosition.distance - pathPosition.position.manhattanDistance(it.position)
+                    saved
+                }
+                .filter { it >= minimumCheatSave }
         }
-        .map{ it.key }
-        .toSet()
-
+        .groupingBy { it }.eachCount()
 }
 
 data class PathPosition(val position: Point, val distance: Int, val end: Point) {
     val minimumExpectedCost = distance + position.manhattanDistance(end)
 
     fun isFinished(): Boolean = position == end
+
+    override fun equals(other: Any?): Boolean {
+        return other is PathPosition && position == other.position
+    }
+
+    override fun hashCode(): Int {
+        return position.hashCode()
+    }
 }
 
